0cbb81ca32db4d5bdbda6b2711869c69e4c4aba9
FrancoBrizuela01 <franco-alexis16@hotmail.com>
2022-06-30 16:16:15 -0300

    Agregar archivos de proyecto.

---
+++ LOGIN_ASP.NET_MVC_SQL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LOGIN_ASP.NET_MVC_SQL.MvcApplication" Language="C#" %>
---
+++ LOGIN_ASP.NET_MVC_SQL/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LOGIN_ASP.NET_MVC_SQL.Models
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+
+        public string ConfirmarClave { get; set; }
+    }
+}
